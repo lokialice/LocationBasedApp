d7bf2b0b071ab154323214e4af4cc174a1a18433
lokialice <lokialicehd@gmail.com>
2015-09-07 11:23:34 +0700

    location based app with Swift

--- LocationBasedApp/ViewController.swift
+++ LocationBasedApp/ViewController.swift
@@ -7,14 +7,53 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,CLLocationManagerDelegate {
+    
+    
+    @IBOutlet var mapView: MKMapView!
+    var lm:CLLocationManager = CLLocationManager()
+    var anotation:MKPointAnnotation = MKPointAnnotation()
+    var geoCoder = CLGeocoder()
+    var location:CLLocation?
+    @IBAction func showLocation(sender: UIButton) {
+        if let location = location {
+            geoCoder.reverseGeocodeLocation(location, completionHandler: {
+                placeMarks, error in
+                if error == nil && !placeMarks.isEmpty {
+                    var placeMark = placeMarks.last as! CLPlacemark
+                    var locationString = "\(placeMark.locality) \(placeMark.postalCode),\(placeMark.country)"
+                    self.anotation.subtitle = locationString
+                }
+            })
+        }
+        
+    }
+    func locationManager(manager: CLLocationManager!, didUpdateToLocation newLocation: CLLocation!, fromLocation oldLocation: CLLocation!) {
+        var span = MKCoordinateSpan(latitudeDelta: 0.001, longitudeDelta: 0.001)
+        var region = MKCoordinateRegion(center: newLocation.coordinate, span: span)
+        mapView.setRegion(region, animated: true)
+        anotation.title = "I am here!"
+        anotation.coordinate = newLocation.coordinate
+        mapView.showAnnotations([anotation], animated: true)
+        location = newLocation
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let authStatus:CLAuthorizationStatus = CLLocationManager.authorizationStatus()
+        if authStatus == .NotDetermined {
+            lm.requestWhenInUseAuthorization()
+            
+        }
+        lm.delegate = self
+        lm.desiredAccuracy = kCLLocationAccuracyBest
+        lm.distanceFilter = kCLDistanceFilterNone
+        lm.startUpdatingLocation()
+        mapView.mapType = MKMapType.Hybrid
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
